f9adaeaf3bbc09adf704bf6687d109b40b9b348e
rachmathikam <katnissx02@gmail.com>
2024-02-12 13:53:24 +0700

    push git

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,10 @@
+<div class="card">
+    <div class="card-header">
+        Hai
+    </div>
+    <div class="card-body">
+        <div class="card-title">Title</div>
+        <p class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint consectetur ratione incidunt exercitationem ea eum corporis atque quis? Repellat nulla odit, nam obcaecati earum nostrum et debitis vero quos illum?</p>
+        <a href="#" class="btn btn-primary">See detail</a>
+    </div>
+</div>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="bg-body-tertiary text-center text-lg-start">
+    <div class="text-center p-3 bg-light">
+        © 2024 Copyright
+        <a href="/" class="nav-link">Blog</a>
+    </div>
+</footer>
